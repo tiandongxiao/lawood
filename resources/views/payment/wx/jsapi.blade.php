1b160051f8d76e39bb664d761015f386c921ebdb
guoying wang <guoying wang>
2016-04-12 17:31:27 +0800

    first

--- resources/views/payment/wx/jsapi.blade.php
+++ resources/views/payment/wx/jsapi.blade.php
@@ -1,6 +1,7 @@
 @extends('base.master')
 @section('content')
-    <font color="#9ACD32"><b>该笔订单支付金额为<span style="color:#f00;font-size:50px">{{$price/100}} 元</span>钱</b></font><br/><br/>
+    <b><span style="color:#c1e2b3;font-size:50px">{{$price/100}} 元</span></b>
+    <br/>
     <div align="center">
         <button style="width:210px; height:50px; border-radius: 15px;background-color:#FE6714; border:0px #FE6714 solid; cursor: pointer;  color:white;  font-size:16px;" type="button" onclick="callpay()" >立即支付</button>
     </div>
