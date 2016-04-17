72473320f01146b5126ada98fbeff0c5e3ed1095
guoying wang <guoying wang>
2016-04-18 00:56:15 +0800

    refactor

--- app/Transaction.php
+++ app/Transaction.php
@@ -7,4 +7,9 @@ use Amsgames\LaravelShop\Models\ShopTransactionModel;
 class Transaction extends ShopTransactionModel
 {
     protected $fillable = ['order_id', 'gateway', 'transaction_id', 'detail', 'token'];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
 }
