2b7d67c83d26556662e9c7c436c52002e8330e19
guoying wang <guoying wang>
2016-04-18 22:30:28 +0800

    refactor

--- resources/views/lawyer/completed.blade.php
+++ resources/views/lawyer/completed.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <ul>
     @foreach($orders as $order)
-        <li><a>订单交易号：{{$order->transactions[0]->transaction_id}}</a>订单状态<span>{{$order->statusCode}}~ 金额：{{$order->total}}</span></li>
+        <li><a>订单交易号：{{$order->transactions[0]->transaction_id}}</a>订单状态<span>{{$order->statusCode}}~ 金额：{{$order->tota/100l}}元</span></li>
     @endforeach
     </ul>
 @endsection
