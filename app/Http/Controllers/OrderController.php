63a37238d54c227052f1ac1872e5d2c3aacf8419
tiandongxiao <team@exingdong.com>
2016-05-17 12:59:21 +0800

    refactor

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Jenssegers\Agent\Facades\Agent;
 use Notify;
 
@@ -20,6 +21,8 @@ class OrderController extends Controller
     # 对未下单的订单下单
     public function placeOrder($id)
     {
+        if(Auth::user())
+            dd(Auth::user());
         $consult = Item::findOrFail($id);
         $seller = $consult->seller;
         if($seller->enable == false)
