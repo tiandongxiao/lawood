d2b0eb9fe7346d263feefc612bc16f8e6a9c0204
tiandongxiao <team@exingdong.com>
2016-05-25 21:06:26 +0800

    改版

--- resources/views/wechat/lawyer/index.blade.php
+++ resources/views/wechat/lawyer/index.blade.php
@@ -3,13 +3,17 @@
     <section class="lvzy-main">
         <h3 class="tie">您附近有<font class="fc-03aaf0">103</font>名律师</h3>
         <p class="sx">输入有<font class="fc-03aaf0">律师姓名</font>或者有<font class="fc-03aaf0">您的位置</font>即可快速查找</p>
-
-
         <form>
-            <a class="itms-form" href="#">
-                <span  class="In-text" >我的位置</span>
-            </a>
-
+            <div class="itms-form" id="form-xz">
+                <div class="bd">
+                    <input type="text" placeholder="请输入律师姓名" id="In-xm" class="In-text">
+                    <input type="text" placeholder="请输入我的位置" id="In-wz" class="In-text" style="display:none">
+                </div>
+                <div class="hd">
+                    <i class="icon-xm on" id="icon-xm"></i>
+                    <i class="icon-wz" id="icon-wz"></i>
+                </div>
+            </div>
             <input type="button" class="In-btn In-btn-1 bg-lan1 fc-fff mar-top-10 fs-16" value="找律师" id="In-btn">
         </form>
 
@@ -49,6 +53,13 @@
 @section('script')
     <script>
         $(function(){
+            //切换查找条件
+            $('#form-xz .hd i').tap(function(){
+                $('#form-xz .hd i').removeClass('on')
+                $(this).addClass('on');
+                $('#form-xz .bd .In-text').css({display:'none'})
+                $('#form-xz .bd .In-text').eq($(this).index()).css({display:'block'})
+            })
 
             $('#In-btn').tap(function () {
                 window.location.href="/wechat/lawyer/search";
