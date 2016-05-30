0045389dfed930c9ba42060503af8230a00e297a
guoying wang <guoying wang>
2016-05-30 22:25:08 +0800

    map

--- resources/views/wechat/lawyer/search.blade.php
+++ resources/views/wechat/lawyer/search.blade.php
@@ -228,8 +228,8 @@
                 map.setCenter(position);
                 //添加点标记，并使用自己的icon
                 var icon = new AMap.Icon({
-                    image: '/images/icon-wz.png',
-                    size: new AMap.Size(44, 56)
+                    image: '/images/icon-wz2.png',
+                    size: new AMap.Size(22, 28)
                 });
                 new AMap.Marker({
                     map: map,
