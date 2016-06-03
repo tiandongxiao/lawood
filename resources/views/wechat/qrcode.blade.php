7c4f68ae994c5c0263191b90f8274574e9bc31df
tiandongxiao <team@exingdong.com>
2016-06-03 12:52:53 +0800

    联动

--- resources/views/wechat/qrcode.blade.php
+++ resources/views/wechat/qrcode.blade.php
@@ -3,6 +3,6 @@
     <div class="te-cen mar-top-40">
         <h3 class="fc-03aaf0" style="font-size: 20px;font-weight: lighter">欢迎您来到律屋</h3>
         <img class="mar-top-50" src="/images/ewm.png" width="65%" height="65%"/>
-        <p class="mar-top-40" style="color: #df8a13">微信扫描二维码,加入我们吧</p>
+        <p class="mar-top-40" style="color: #df8a13">微信扫描二维码  加入我们</p>
     </div>
 @stop
