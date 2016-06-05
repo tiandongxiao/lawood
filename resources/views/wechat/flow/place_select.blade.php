1e67e59bda7278588f19d3529d444d120e7631f0
guoying wang <guoying wang>
2016-06-05 16:53:21 +0800

    doing

--- resources/views/wechat/flow/place_select.blade.php
+++ resources/views/wechat/flow/place_select.blade.php
@@ -5,7 +5,7 @@
     <!--约见地点-->
     <div class="tc-m yjdd-main" style="display: block;width:100%;left: 0;height: 100%;background-color: white;">
         <div class="bg-fff c-main" style="max-height: 100%;height: 100%;border-radius: 0;">
-            <div class="tie" style="width:100%;background-color: #00abeb;color: white">选择约见地点</div>
+            <div class="tie" style="width:100%;background-color: #00abeb;color: white;border-radius: 0">选择约见地点</div>
             <div class="con" >
                 <div class="line-40 " style="color: #F39D2E">提示：下列地点仅为推荐，可协商变更</div>
                 <div class="itms">
