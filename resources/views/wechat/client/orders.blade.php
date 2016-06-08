9a4646ac5722c70aaed0f1b930b86b91506dfb02
guoying wang <guoying wang>
2016-06-08 23:25:34 +0800

    refresh

--- resources/views/wechat/client/orders.blade.php
+++ resources/views/wechat/client/orders.blade.php
@@ -147,7 +147,7 @@
                         @endif
                     @endforeach
                 @else
-                    <div style="text-align:center;">您当前没有未完成的订单</div>
+                    <div style="text-align:center;margin-top: 60%">您当前没有未完成的订单</div>
                 @endif
             </div>
             <!--未完成-->
@@ -209,7 +209,7 @@
                         @endif
                     @endforeach
                 @else
-                    <div style="text-align:center;">您当前没有进行中的订单</div>
+                    <div style="text-align:center;margin-top: 60%">您当前没有进行中的订单</div>
                 @endif
             </div>
             <!--进行中-->
@@ -243,7 +243,7 @@
                         </div>
                     @endforeach
                 @else
-                    <div style="text-align:center;">您当前没有进行中的订单</div>
+                    <div style="text-align:center;margin-top: 60%">您当前没有进行中的订单</div>
                 @endif
             </div>
             <!--已完成-->
