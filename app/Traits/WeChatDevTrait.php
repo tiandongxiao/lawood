b0d69594de3e7f6e606a1dc09573bf48e583ddb6
guoying wang <guoying wang>
2016-06-08 18:12:21 +0800

    refresh

--- app/Traits/WeChatDevTrait.php
+++ app/Traits/WeChatDevTrait.php
@@ -77,6 +77,7 @@ trait WeChatDevTrait
     public function account()
     {
         $user = session('wechat.oauth_user');
+        dd($user);
         if ($user) {
             Log::info('account 获取成功');
             $accessToken = $this->app->access_token;
