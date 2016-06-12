d1aa5b4787ff04e15c9f9e31f70cbb9b647b24f7
guoying wang <guoying wang>
2016-06-12 15:56:11 +0800

    order

--- resources/views/wechat/client/orders.blade.php
+++ resources/views/wechat/client/orders.blade.php
@@ -104,14 +104,20 @@
                                         <p class="name chaochu_1">律师 {{$order->seller->real_name}}<span>婚姻</span></p>
                                         <p class="dd chaochu_2">预约地点：{{$order->place->name}}</p>
                                         <p class="jl chaochu_1">距离：0.9km</p>
+                                        @if($order->payed)
                                         <div class="zxf">
                                             <p>已退咨询费</p>
                                             <p class="jg">{{$order->total}}元</p>
                                         </div>
+                                        @endif
                                     </div>
                                 </div>
                                 <div class="bottom">
-                                    <div class="djs fc-03aaf0 fs-12">订单已取消，费用已退回</div>
+                                    @if($order->payed)
+                                        <div class="djs fc-03aaf0 fs-12">订单已取消，费用已退回</div>
+                                    @else
+                                        <div class="djs fc-03aaf0 fs-12">订单已取消</div>
+                                    @endif
                                     <div class="btn-main">
                                         <span class="btn lan"><a href="{{url('wechat')}}">重新查找</a></span>
                                     </div>
