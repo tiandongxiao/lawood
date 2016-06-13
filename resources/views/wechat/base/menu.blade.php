5c9ec3262d64b1efac48476f5b111dc56a1a9a31
guoying wang <guoying wang>
2016-06-13 22:05:08 +0800

    order

--- resources/views/wechat/base/menu.blade.php
+++ resources/views/wechat/base/menu.blade.php
@@ -145,9 +145,13 @@
             <div class="f-left"><img src="/images/nav2.png" width="20" height="20"></div>
             <div class="right">消息通知</div>
         </a>
-        <a class="itms" href="{{url('wx/orders')}}">
+        <a class="itms bor-bot" href="{{url('wx/orders')}}">
             <div class="f-left"><img src="/images/nav3.png" width="20" height="20"></div>
             <div class="right">我的订单</div>
+        </a>
+        <a class="itms" href="{{url('wechat/chose')}}">
+            <div class="f-left"><img src="/images/nav4.png" width="20" height="20"></div>
+            <div class="right">加入律屋</div>
         </a>
     @endif
     </div>
