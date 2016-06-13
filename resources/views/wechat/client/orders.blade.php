c735012b25cac14ebaaad3bd7b7aac5023b6019e
guoying wang <guoying wang>
2016-06-13 14:53:17 +0800

    order

--- resources/views/wechat/client/orders.blade.php
+++ resources/views/wechat/client/orders.blade.php
@@ -385,17 +385,17 @@
     <script>
         $(function(){
             //检查版本
-            var u = navigator.userAgent, app = navigator.appVersion;
-            var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-
-            //苹果兼容
-            if(isiOS){
-                $('.In-text').focus(function(){
-                    $('.po-f').addClass('po-a');
-                }).blur(function(){//输入框失焦后还原初始状态
-                    $('.po-f').removeClass('po-a')
-                });
-            }
+//            var u = navigator.userAgent, app = navigator.appVersion;
+//            var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+//
+//            //苹果兼容
+//            if(isiOS){
+//                $('.In-text').focus(function(){
+//                    $('.po-f').addClass('po-a');
+//                }).blur(function(){//输入框失焦后还原初始状态
+//                    $('.po-f').removeClass('po-a')
+//                });
+//            }
 
             // timing rating
             $('#time span').tap(function () {
