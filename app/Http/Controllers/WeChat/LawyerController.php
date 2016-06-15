6a82e1393b7f34d98e5b1877ca90bfa560489383
tiandongxiao <team@exingdong.com>
2016-06-15 16:37:29 +0800

    refactor

--- app/Http/Controllers/WeChat/LawyerController.php
+++ app/Http/Controllers/WeChat/LawyerController.php
@@ -80,13 +80,13 @@ class LawyerController extends Controller
 
     public function postDraw(Request $request)
     {
-        $name = trim($request->get('name'));
-        $phone = trim($request->get('phone'));
-        $card = trim($request->get('card'));
-        $code = trim($request->get('code'));
-
-        if($phone != $this->user->phone || $name != $this->user->real_name)
-            return back();
+//        $name = trim($request->get('name'));
+//        $phone = trim($request->get('phone'));
+//        $card = trim($request->get('card'));
+//        $code = trim($request->get('code'));
+//
+//        if($phone != $this->user->phone || $name != $this->user->real_name)
+//            return back();
         $orders = $this->user->not_drew_orders;
         dd($orders);
 //        if($code == Cache::get('check_'.$phone)){
