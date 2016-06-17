468f84f8aa0a464766316dc8401c8c1c740e9d42
guoying wang <guoying wang>
2016-06-17 15:45:52 +0800

    refactor

--- resources/views/wechat/lawyer/config/share.blade.php
+++ resources/views/wechat/lawyer/config/share.blade.php
@@ -27,6 +27,6 @@
 @section('script')
     <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
     <script type="text/javascript" charset="utf-8">
-        wx.config(<?php echo $js->config(array('onMenuShareQQ', 'onMenuShareWeibo'), true) ?>);
+        wx.config(<?php echo $js->config(array('onMenuShareQQ', 'onMenuShareWeibo'), false) ?>);
     </script>
 @stop
