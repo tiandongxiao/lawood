d1a2b9520b19872bba182c1b0ca90fed4a335309
guoying wang <guoying wang>
2016-06-21 11:26:04 +0800

    share

--- resources/views/wechat/client/orders.blade.php
+++ resources/views/wechat/client/orders.blade.php
@@ -392,7 +392,7 @@
                 <input type="hidden" name="order" id="m-order" value=""/>
                 <input type="hidden" name="user-score" id="m-score" value="5"/>
                 <div class="pjyj  pad-0-10 mar-top-10">
-                    <textarea class="In-text" name="comment" id="m-comment" style="height: 55px;"></textarea>
+                    <textarea class="In-text" name="comment" id="m-comment" style="height: 55px;margin-bottom: 20px"></textarea>
                 </div>
             </form>
             <input type="button" class="In-btn In-btn-1 bg-lan1 fc-fff mar-top-10"  value="提交" id="rate-modify">
