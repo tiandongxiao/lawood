451bafccb4acc4a72c2af4ebf3aa3220c91a22d4
guoying wang <guoying wang>
2016-06-24 00:37:54 +0800

    share

--- resources/views/bill/edit.blade.php
+++ resources/views/bill/edit.blade.php
@@ -1,10 +1,11 @@
 @extends('tpl.admin.app')
 @section('content')
-    <h2>Update Bill: {{$bill->user_id}}</h2>
+    <h2>设置账单是否完成: {{$bill->name}}律师的提款申请</h2>
+    <p>提款金额：{{$bill->amount}}</p>
     <form action="/bill/{{$bill->id}}" method="post">
         {{ csrf_field() }}
         {{ method_field("PUT") }}
-        {!! \Nvd\Crud\Form::input('done','boolean')->model($bill)->show() !!}
-        <button type="submit" class="btn btn-default">Submit</button>
+        {!! \Nvd\Crud\Form::input('done','bool')->model($bill)->show() !!}
+        <button type="submit" class="btn btn-default">设置</button>
     </form>
 @endsection
