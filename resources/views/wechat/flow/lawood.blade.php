62f1cfcb7957fde083f9ef3b6baa21740df1fb93
guoying wang <guoying wang>
2016-06-23 15:18:28 +0800

    share

--- resources/views/wechat/flow/lawood.blade.php
+++ resources/views/wechat/flow/lawood.blade.php
@@ -6,8 +6,8 @@
     <section class="lssz-main">
         <div class="te-cen">
             <p class="fs-16 fc-03aaf0  mar-top-20 line-20">欢迎来到律屋在线</p>
-            <div class="mar-top-15 mar-top-10">
-                <img src="/images/ewm.png" width="235px" height="235" style="padding: 10px;background-color: #9d9d9d">
+            <div class="mar-top-15 mar-top-15">
+                <img src="/images/ewm.png" width="200px" height="200" style="padding: 10px;border: dotted 1px #00a7d0 ">
                 <p class="mar-top-20" style="font-weight: lighter;font-size: 12px;">微信中长按二维码或微信搜索'律屋'加入我们</p>
             </div>
             <div class="bottom-btn">
