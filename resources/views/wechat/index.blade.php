d80ca70d8741d57b9be8e9a0c8c89148ac029429
guoying wang <guoying wang>
2016-06-25 00:44:16 +0800

    share

--- resources/views/wechat/index.blade.php
+++ resources/views/wechat/index.blade.php
@@ -28,7 +28,7 @@
         </form>
 
         <div class="line-30 pad-0-10 fc-909090 mar-top-50" style="text-align: center"><span style="color: rgba(255,152,0,0.78);font-size: 16px">推 荐 律 师</span></div>
-        <div class="tjls pad-0-10  mar-top-15 clearfix">
+        <div class="tjls pad-0-10  mar-top-20 clearfix">
             <a class="itms" href="{{url('wechat/user/6')}}">
                 <div class="img"><img src="/images/ls.jpg" width="100%"></div>
                 <div class="banner">
