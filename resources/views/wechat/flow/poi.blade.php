13e7f041e00b9a8efac616be0678bd2608d5e3dd
guoying wang <guoying wang>
2016-06-28 23:00:03 +0800

    index

--- resources/views/wechat/flow/poi.blade.php
+++ resources/views/wechat/flow/poi.blade.php
@@ -41,7 +41,7 @@
                 position: [position.getLng(), position.getLat()],
                 icon: new AMap.Icon({
                     size: new AMap.Size(48, 48),  //图标大小
-                    image: "/images/poi.svg"
+                    image: "/images/poi_coffee.svg"
                 })
             });
         }
