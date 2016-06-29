d52ab8d57d8e3d6cca03097ffc30294f232eb16c
guoying wang <guoying wang>
2016-06-29 19:02:50 +0800

    index

--- resources/views/wechat/base/service/gaode.blade.php
+++ resources/views/wechat/base/service/gaode.blade.php
@@ -1,17 +1,5 @@
 {{--<script src="http://webapi.amap.com/maps?v=1.3&key=b6f97a31076e886a1236312d87e8b35e"></script>--}}
 <script>
-
-    function loadGaode() {
-        jQuery.getScript("http://webapi.amap.com/maps?v=1.3&key=b6f97a31076e886a1236312d87e8b35e").done(function() {
-            alert('成功');
-            gdMapInit();
-        }).fail(function() {
-            alert('失败');
-            loadGaode();
-        });
-    }
-    loadGaode();
-
     var map, cur_location, keyword, cloudDataLayer;
 
     function gdMapInit() {
