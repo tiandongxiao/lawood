8ca457c4b3553d925724aa93e2448ba9109c4d30
guoying wang <guoying wang>
2016-07-03 14:25:18 +0800

    lawyer index

--- resources/views/wechat/auth/chose.blade.php
+++ resources/views/wechat/auth/chose.blade.php
@@ -1,10 +1,18 @@
 @extends('wechat.base.app')
 @section('css')
-    <style>body{background:#03aaf0}</style>
+    <style>
+        body{background:#03aaf0}
+        .dl-main .logo-index {
+            margin: 25% 0;
+        }
+        .dl-main .bottom-index {
+            bottom: 25%;
+        }
+    </style>
 @endsection
 @section('content')
     <section class="dl-main">
-        <div class="logo-index"><img src="/images/logo.png"></div>
+        <div class="logo-index"><img style="width: 70%" src="/images/logo.png"></div>
         <div class="fc-fff bottom-index">
             <a href="{{url('wechat/bind/client')}}">个人注册</a>　|　<a href="{{url('wechat/bind/lawyer')}}">律师注册</a>
         </div>
