341380e5a9c64a42834a20166423294cf989db55
guoying wang <guoying wang>
2016-07-04 15:23:10 +0800

    share

--- resources/views/wechat/client/orders.blade.php
+++ resources/views/wechat/client/orders.blade.php
@@ -83,7 +83,7 @@
                                 <div class="bottom">
                                     <div class="djs fc-03aaf0 fs-12">正在等待律师接单…</div>
                                     <div class="btn-main">
-                                        <span class="btn"><a href="{{url('wechat/order/cancel/'.$order->id)}}">取消订单</a></span>
+                                        <span class="btn btn-cancel" data-order="{{$order->id}}" data-client="{{$order->user->id}}">取消订单</span>
                                     </div>
                                 </div>
                             </div>
