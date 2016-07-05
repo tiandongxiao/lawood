0bbbcc76c1c60bd0784e8e5361db51264c24c288
tiandongxiao <team@exingdong.com>
2016-07-06 00:44:44 +0800

    pc

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,13 +8,28 @@
         <link rel="shortcut fa" href="favfa.png">
         <LINK href="/css/pc_global.css" rel="stylesheet" type="text/css">
         <LINK href="/css/pc_css.css" rel="stylesheet" type="text/css">
+        <style>
+            .banner-index	.btn-xz {
+                background: #419AE6;
+            }
+            .banner-index	.btn-xz:hover {
+                background: #419AE6;
+            }
+            a {
+                text-decoration: none;
+                color: #e49840;
+            }
+            .fc-coral{
+                color: lightcoral;
+            }
+        </style>
     </head>
     <body>
         <div class="nav">
             <div class="w1250">
                 <div class="logo"><img src="/images/logo_pc.png" width="250" height="100"></div>
                 <div class="f-right">
-                    <a href="/" class="itms">首页</a>
+                    <a href="/" class="itms on">首页</a>
                     <a href="/flow" class="itms">律师人口</a>
                     <a href="/about" class="itms">关于律屋</a>
                 </div>
@@ -23,10 +38,10 @@
         <div class="banner-index">
             <div class="w1250">
                 <div class="top">
-                    <p style="font-size: 26px;letter-spacing: 2px">与律师面对面</p>
-                    <p style="font-size: 26px;letter-spacing: 2px;margin-top: 10px">体验<span> 高效、便捷、专业 </span>的法律咨询</p>
+                    <p style="font-size: 26px;letter-spacing: 3px">与律师面对面</p>
+                    <p style="font-size: 26px;letter-spacing: 2px;margin-top: 10px">体验<span> 高效、便捷、专业 </span>的咨询服务</p>
                 </div>
-                <a href="#" class="btn-xz huandong" style="margin-top:200px">找到您身边的律师</a>
+                <div href="#" class="btn-xz huandong" style="margin-top:200px;letter-spacing: 1px">找到您身边的律师</div>
                 <div class="ewm">
                     <img src="images/ewm_pc.png" width="321" height="271">
                     <div class="sys">扫一扫，约律师</div>
@@ -62,41 +77,31 @@
                 <div class="itms">
                     <img src="/images/20.png" width="117" height="133">
                     <div class="font">
-                        <p>债券债务债券债务</p>
-                        <p>洽谈专业洽谈专业洽谈专业洽谈专业</p>
-                        <p>债券债务债券债务债券债务</p>
+                        <p>专业的律师团队</p>
                     </div>
                 </div>
                 <div class="itms">
                     <img src="/images/21.png" width="117" height="133">
                     <div class="font">
-                        <p>债券债务债券债务</p>
-                        <p>洽谈专业洽谈专业洽谈专业洽谈专业</p>
-                        <p>债券债务债券债务债券债务</p>
+                        <p>安全的信息保障</p>
                     </div>
                 </div>
                 <div class="itms">
                     <img src="/images/22.png" width="117" height="133">
                     <div class="font">
-                        <p>债券债务债券债务</p>
-                        <p>洽谈专业洽谈专业洽谈专业洽谈专业</p>
-                        <p>债券债务债券债务债券债务</p>
+                        <p>透明的咨询费用</p>
                     </div>
                 </div>
                 <div class="itms">
                     <img src="/images/23.png" width="117" height="133">
                     <div class="font">
-                        <p>债券债务债券债务</p>
-                        <p>洽谈专业洽谈专业洽谈专业洽谈专业</p>
-                        <p>债券债务债券债务债券债务</p>
+                        <p>严格的律师审核</p>
                     </div>
                 </div>
                 <div class="itms">
                     <img src="/images/24.png" width="117" height="133">
                     <div class="font">
-                        <p>债券债务债券债务</p>
-                        <p>洽谈专业洽谈专业洽谈专业洽谈专业</p>
-                        <p>债券债务债券债务债券债务</p>
+                        <p>专业的客户服务</p>
                     </div>
                 </div>
             </div>
--- resources/views/site/flow.blade.php
+++ resources/views/site/flow.blade.php
@@ -17,60 +17,10 @@
                 top: 75px;
                 background: rgba(216, 134, 11, 0.81);
             }
-            .list-zc-tie	h3 {
-                font-size: 46px;
-                color: rgba(5, 108, 236, 0.67);
-                font-weight: normal;
-            }
             .list-zc-tie	P {
-                font-size: 20px;
+                font-size: 16px;
                 color: rgba(226, 0, 0, 0.64);
                 line-height: 50px;
-            }
-            .banner-rk .top {
-                padding-top: 230px;
-                color: #fff;
-                font-size: 16px;
-                height: 35px;
-                line-height: 35px;
-                opacity: 0.8;
-            }
-            .banner-rk .btn-xz:hover {
-                background: #e57300;
-            }
-            .banner-rk .btn-xz {
-                display: block;
-                width: 321px;
-                height: 55px;
-                background: #e69941;
-                border-radius: 2px;
-                line-height: 55px;
-                text-align: center;
-                color: #fff;
-                font-size: 26px;
-                margin-top: 130px;
-            }
-            .banner-rk .ewm {
-                margin-top: 15px;
-                position: relative;
-            }
-            .banner-rk .sys {
-                width: 121px;
-                height: 31px;
-                line-height: 31px;
-                text-align: center;
-                color: #D9D9D9;
-                font-size: 12px;
-                background: url(../images/yxjj.png) center center no-repeat;
-                position: absolute;
-                bottom: 10px;
-                left: 100px;
-            }
-            .banner-rk .top	span {
-                font-size: 28px;
-                color: #e49840;
-                font-style: italic;
-                margin: 0 5px;
             }
         </style>
     </head>
@@ -86,17 +36,6 @@
             </div>
         </div>
         <div class="banner-rk">
-            <div class="w1250">
-                <div class="top">
-                    <p>与律师面对面</p>
-                    <p>体验<span> 高效、便捷、专业 </span>的法律咨询</p>
-                </div>
-                <a href="#" class="btn-xz huandong">律屋网欢迎您</a>
-                <div class="ewm">
-                    <img src="images/ewm_pc.png" width="321" height="271">
-                    <div class="sys">扫一扫，加入律屋</div>
-                </div>
-            </div>
         </div>
         <div class="list-zc-tie">
             <h3>律师注册流程</h3>
@@ -131,18 +70,26 @@
         <div class="list-phone">
             <div class="itms">
                 <p class="top">律师注册</p>
-                <div class="ewm"><img src="images/ewm-1.png"></div>
+                <div class="ewm"><img src="images/lawood.jpg" width="226px" height="221px"></div>
                 <div class="bottom">扫一扫，马上注册律师</div>
             </div>
             <div class="itms">
-                <p class="top">修改资料</p>
-                <div class="ewm"><img src="images/ewm-1.png"></div>
-                <div class="bottom">扫一扫，马上修改资料</div>
+                <p class="top">修改简介</p>
+                <div class="ewm btn-wx"><img src="images/click.png" width="226px" height="221px"></div>
+                <div class="bottom btn-wx">点一点，扫码后改资料</div>
             </div>
         </div>
         <div class="footer">
             <p><span>Tel:123456789</span>　　<span>Emil:123456789@qq.com</span></p>
             <p>北京律屋网络服务有限公司</p>
         </div>
+        <script src="//cdn.bootcss.com/jquery/2.2.0/jquery.min.js"></script>
+        <script>
+            $(function(){
+                $('.btn-wx').click(function () {
+                    window.location.href="/wx/login";
+                });
+            })
+        </script>
     </body>
 </html>
