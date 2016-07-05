dccbb3c1845ba0a867cb678366e8c3b3ffc630b1
tiandongxiao <team@exingdong.com>
2016-07-06 01:10:23 +0800

    pc

--- resources/views/site/flow_edit.blade.php
+++ resources/views/site/flow_edit.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+o<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -18,6 +18,7 @@
                 color: #F8F8F8;
                 line-height: 47px;
                 text-align: center;
+                border-radius: 6px;
             }
         </style>
     </head>
