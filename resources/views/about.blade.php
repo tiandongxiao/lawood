079e2ab7668dc1037d403af1376ed00689910cfd
tiandongxiao <team@exingdong.com>
2016-07-07 12:23:10 +0800

    pc

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,8 +6,8 @@
         <meta name="keywords" content="律屋">
         <meta name="description" content="律屋">
         <link rel="shortcut fa" href="favfa.png">
-        <LINK href="/css/pc_global.css" rel="stylesheet" type="text/css">
-        <LINK href="/css/pc_css.css" rel="stylesheet" type="text/css">
+        <link href="/css/pc_global.css" rel="stylesheet" type="text/css">
+        <link href="/css/pc_css.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="nav">
@@ -23,23 +23,13 @@
         <div class="banner-rk"></div>
         <div class="about-gz">
             <h3 class="t1">专注北京 多元化的律师选择</h3>
-            <p class="font2" style="width: 100%">律屋(北京)网络服务有限公司，是一家专注于法律咨询业务的互联网平台，律屋自2016年2月筹备至今，我们已经得到200多家北京律所400多名律师的认可并加入了我们的平台。在北京，每5名律师就有一名律师是我们的签约律师。律屋专注于小微法律咨询，咨询范围涉及婚姻家庭、房产纠纷、债务合同、劳动争议、公司事务等各个领域，并广泛分布于北京各个城区，律屋专业律师竭诚为您带来便捷、高效、专业、亲和的法律咨询服务。</p>
+            <p class="font2">律屋（北京）网络服务有限公司，是一家专注于法律咨询业务的互联网平台，律屋自2016年2月筹备至今，我们已经得到200多家北京律所400多名律师的认可并加入了我们的平台。在北京，每5名律师就有一名律师是我们的签约律师。律屋专注于小微法律咨询，咨询范围涉及婚姻家庭、房产纠纷、债务合同、劳动争议、公司事务等各个领域，并广泛分布于北京各个城区，律屋专业律师竭诚为您带来便捷、高效、专业、亲和的法律咨询服务。</p>
         </div>
         <div class="about-img1 te-cen" style="padding-top: 100px"><img src="/images/30.jpg"></div>
         <div class="about-pp" style="padding-top: 100px;padding-bottom: 100px">
             <h3 class="t1">老百姓的律师品牌</h3>
             <p class="font2">身处互联网时代的今天，并没有一家律师平台深入人心，而打造一个这样的品牌成为了我们公司和平台律师共同的目标。律屋是律师和用户的家,律屋团队更多的就是全方位服务律师和咨询用户。律屋对律师严格筛选，并对非诚信律师零容忍。当您聘请平台律师进行法律咨询服务时，律屋不会让您失望，您将获得一个极致的服务体验。</p>
         </div>
-        {{--<div class="about-sp">--}}
-            {{--<img src="/images/video.jpg">--}}
-        {{--</div>--}}
-        {{--<div class="about-td" style="margin-bottom: 100px">--}}
-            {{--<h3 class="t1">律师团队</h3>--}}
-            {{--<div class="font2">--}}
-                {{--<p>律屋由律师，IT精英，新闻媒体人共同发起设立，不同的专业分工和协作将使我们的平台更贴近用户和律师的需求，律师推广的成本也更低。</p>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="about-img" style="margin-bottom: -100px"></div>--}}
         <div class="footer">
             <p><span>Tel : 123456789</span>　　<span>Emil : kefu@lawood.cn</span></p>
             <p>北京律屋网络服务有限公司</p>
